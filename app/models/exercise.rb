fba7535490712b1f5e79ee1bd68922d191f78552
kamkara <lnclass.kamkara@gmail.com>
2022-11-03 17:14:31 +0000

    Add exercise and Question and Answer feature basic

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -2,5 +2,14 @@ class Exercise < ApplicationRecord
   belongs_to :user
   belongs_to :course
 
-  
+  has_many :questions, dependent: :destroy
+    accepts_nested_attributes_for :questions, allow_destroy: true
+
+
+    extend FriendlyId
+    friendly_id :name, use: :slugged
+
+  def should_generate_new_friendly_id?
+    name_changed?
+  end
 end
---
+++ db/migrate/20221103170329_create_questions.rb
@@ -0,0 +1,12 @@
+class CreateQuestions < ActiveRecord::Migration[7.0]
+  def change
+    create_table :questions, id: :uuid do |t|
+      t.string :name
+      t.integer :question_type
+      t.boolean :required
+      t.belongs_to :exercise, null: false, foreign_key: true, type: :uuid
+
+      t.timestamps
+    end
+  end
+end
