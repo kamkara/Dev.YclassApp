dc47c23f81e51b366d29b89ef44bf2aa6e0e3fe6
kamkara <lnclass.kamkara@gmail.com>
2022-11-03 17:00:17 +0000

    Add exrcise

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,6 +2,9 @@ class Course < ApplicationRecord
   belongs_to :user
   has_rich_text :content
 
+  has_many :exercises, class_name: "Exercise", foreign_key: "course_id", dependent: :destroy
+
+
   extend FriendlyId
     friendly_id :title, use: :slugged
 
---
+++ app/models/exercise.rb
@@ -0,0 +1,6 @@
+class Exercise < ApplicationRecord
+  belongs_to :user
+  belongs_to :course
+
+  
+end
